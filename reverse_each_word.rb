6923358051389357149c6b89567a5020d60efb15
Jack Robbins <jackrobbins1@gmail.com>
2019-03-20 16:40:25 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -8,6 +8,6 @@
 def reverse_each_word(str)
   arr = str.split(" ")
   newArr = []
-  arr.each { |el| newArr << el.reverse }
+  arr.collect { |el| newArr << el.reverse }
   return newArr.join(" ")
 end
