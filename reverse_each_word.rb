6f8c46c3be383e48e4f84b2df899a556462f67c6
Jack Robbins <jackrobbins1@gmail.com>
2019-03-20 16:33:54 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(str)
   arr = str.split(" ")
-  print arr.each { |el| el.reverse }.join
+  print arr.each { |el| el.reverse }.join(" ")
 end
 
 reverse_each_word("This is a test")
