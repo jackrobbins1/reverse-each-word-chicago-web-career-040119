f0cf2cb5a035f69a6598666f1f34d28669162e60
Jack Robbins <jackrobbins1@gmail.com>
2019-03-20 16:41:25 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,12 @@
+# def reverse_each_word(str)
+#   arr = str.split(" ")
+#   newArr = []
+#   arr.each { |el| newArr << el.reverse }
+#   return newArr.join(" ")
+# end
+
+def reverse_each_word(str)
+  arr = str.split(" ")
+  newArr = arr.collect { |el| el.reverse }
+  return newArr.join(" ")
+end
